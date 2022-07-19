df8ee9ddf39fdc15d34eea1731184fb05a4b59b5
Tim <tim.chao0825@gmail.com>
2022-07-19 20:52:08 +0800

    review composition api function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,70 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      test123 For a guide and recipes on how to configure / customize this
-      project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>Option API :</h1>
+    <h2>{{ count }}</h2>
+    <button @click="addCount">Add Count</button>
     <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
+      <li v-for="(name, idx) in countList" :key="idx">
+        {{ name }}
       </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h2>count * countList = {{ increaseCountList }}</h2>
+    <hr />
+    <h1>Composition API :</h1>
+    <h2>{{ setupCount }}</h2>
+    <button @click="setupAddCount">Add Count</button>
     <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      <li v-for="(name, idx) in setupCountList" :key="idx">
+        {{ name }}
       </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h2>setupCount * setupCountList = {{ setupIncreaseCountList }}</h2>
   </div>
 </template>
 
 <script>
+import { ref, computed } from "vue";
+// ref == reactive reference
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  // ---
+  // option API
+  // ---
+  data() {
+    return {
+      count: 1,
+      countList: ["a", "b", "c"],
+    };
+  },
+  computed: {
+    increaseCountList() {
+      return this.count * this.countList.length;
+    },
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+    },
+  },
+  // ---
+  // Composition API
+  // ---
+  setup() {
+    const setupCount = ref(10);
+    const setupCountList = ref(["d", "e", "f", "g"]);
+    const setupIncreaseCountList = computed(() => {
+      return setupCount.value * setupCountList.value.length;
+    });
+    function setupAddCount() {
+      setupCount.value++;
+    }
+
+    return {
+      setupCount,
+      setupAddCount,
+      setupCountList,
+      setupIncreaseCountList,
+    };
   },
 };
 </script>
